12d475de07f73b6498ac4e8748f8257ca57c581e
lalit-03 <rlalitchandra99@gmail.com>
2023-07-20 19:57:16 +0530

    minor changes

--- checkpoints/BJAir/hierarchical_PM25_20230706T034043/run_test.sh
+++
@@ -1,2 +0,0 @@
-source activate pytorch-py39
-python test.py --model hierarchical --dataset_mode BJAir --pred_attr PM25_Concentration --gpu_ids 0 --config config1 --file_time 20230706T034043 --epoch best
